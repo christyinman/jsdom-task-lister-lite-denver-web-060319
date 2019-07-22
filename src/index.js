6adcc4558822f7fd8e2c67e86522da5de16ed41d
github username <github email address>
2019-07-22 10:54:58 -0600

    solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.querySelector('#create-task-form')
+  const taskList = document.querySelector('#tasks')
+  const newTaskDescription = document.querySelector('#new-task-description')
+  
+  
+  form.addEventListener('submit', addTaskToList)
+  
+  
+  function addTaskToList(event) {
+    event.preventDefault()
+    const newTask = document.createElement('li')
+    newTask.innerText = newTaskDescription.value
+    appendNewTask(newTask)
+    event.target.reset()
+  }
+  function appendNewTask(task) {
+    taskList.appendChild(task)
+  }
 });
